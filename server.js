92d4bafd9ec6dcd58c098db3b5009486362c613b
AllyssaA <a.alimoestar@gmail.com>
2023-05-12 22:18:05 +0200

    styling changes, code changes

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-/*
-https://socket.io/get-started/chat
-*/
-
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
@@ -10,59 +6,51 @@ const io = require("socket.io")(http);
 const fetch = require("node-fetch");
 const port = process.env.PORT || 5000;
 
-// const historySize = 50;
-// let history = [];
+let triviaQuestions = [];
+let currentQuestion = null;
+const scores = {};
 
-//  Templating files
+// Templating files
 app.set("views", "views");
-
 app.set("view engine", "ejs");
-
 app.use(express.static(path.resolve("static")));
 
-// // index route
-// app.get("/", (req, res) => {
-//   res.render("index", {
-//     pageTitle: "Chat",
-//   });
-// });
+// Fisher-Yates algorithm weeee
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
-// const renderPage
-
-// io.on("connection", (socket) => {
-//   console.log("a user connected");
-//   io.emit("history", history);
-
-//   socket.on("message", (message) => {
-//     while (history.length > historySize) {
-//       history.shift();
-//     }
-//     history.push(message);
-
-//     io.emit("message", message);
-//   });
-
-//   socket.on("disconnect", () => {
-//     console.log("user disconnected");
-//   });
-// });
-
-// http.listen(port, () => {
-//   console.log("listening on port ", port);
-// });
-
-// TODO: trivia questions
 const renderPage = (req, res) => {
   try {
-    fetch("https://opentdb.com/api.php?amount=30&category=11")
+    fetch("https://opentdb.com/api.php?amount=5&category=11")
       .then((response) => response.json())
-      .then((data) =>
+      .then((data) => {
+        data.results.forEach((result) => {
+          const allAnswers = shuffleArray([result.correct_answer].concat(
+            result.incorrect_answers)
+          );
+          const question = {
+            question: result.question,
+            correctAnswer: result.correct_answer,
+            incorrectAnswers: result.incorrect_answers,
+            allAnswers: allAnswers,
+          };
+          triviaQuestions.push(question);
+          console.log(triviaQuestions);
+        });
+        // triviaQuestions = data.results;
+        // console.log(triviaQuestions);
         res.render("index", {
-          data: data.results,
+          data: triviaQuestions,
           userList,
-          pageTitle: "Chat | hi",
-        })
-      );
+          pageTitle: "Chat | Trivia",
+          scores,
+        });
+      });
   } catch (err) {
     res.render("error", {
       pageTitle: "Error",
@@ -72,38 +60,102 @@ const renderPage = (req, res) => {
 
 app.get("/", renderPage);
 
-const userList = []
+const userList = [];
 
-io.on('connection', socket => {
-  console.log('a user connected')
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-  const handleName = name => {
-    const user = { username: name, id: socket.id }
-    userList.push(user)
-    console.log(userList)
-    io.emit('name', { username: name, id: socket.id })
-  }
+  selectRandomQuestion();
+  socket.emit("newQuestion", currentQuestion)
 
-  const handleDisconnect = () => {
-    io.emit('user left', { id: socket.id })
+  socket.on("newMsg", (message) => {
+    io.emit("sendMessage", {
+      message: message,
+      user: socket.username,
+    });
+  });
 
-    userList.filter(element => {
-      if (element.id !== socket.id) {
-        
-        return true
+  socket.on("newUser", (user) => {
+    socket.username = user;
+    userList.push(user);
+    console.log("user joined the chat - : " + socket.username);
+    console.log("All connected users " + userList);
+    io.emit("updateUserList", userList);
+  });
+
+  socket.on("answerSelected", (answer) => {
+    if (currentQuestion.correctAnswer === answer) {
+      // Update the score for the user who answered correctly
+      if (!scores[socket.username]) {
+        scores[socket.username] = 1;
       } else {
-  
-        return false
+        scores[socket.username]++;
       }
-    })
-    console.log('user disconnected')
+  
+      io.emit("correctAnswer", {
+        user: socket.username,
+        answer: answer,
+        scores: scores
+      });
+  
+      sendNextQuestion();
+    }
+  });
+  
+
+  function sendNextQuestion() {
+    // Remove the current question from the triviaQuestions array
+    triviaQuestions.splice(triviaQuestions.indexOf(currentQuestion), 1);
+  
+    if (triviaQuestions.length === 0) {
+      // If there are no more questions, end the game
+      gameOver();
+    } else {
+      // Get a random question from the triviaQuestions array
+      const randomIndex = Math.floor(Math.random() * triviaQuestions.length);
+      currentQuestion = triviaQuestions[randomIndex];
+  
+      io.emit("newQuestion", currentQuestion);
+    }
   }
 
-  socket.on('name', handleName)
-  socket.on('disconnect', handleDisconnect)
-  // console.log(userList)
-})
+  socket.on("disconnect", () => {
+    const index = userList.indexOf(socket.username);
+    if (index !== -1) {
+      userList.splice(index, 1);
+      io.emit("updateUserList", userList);
+    }
+    console.log("user disconnected");
+  });
+});
+
+function selectRandomQuestion() {
+  const randomIndex = Math.floor(Math.random() * triviaQuestions.length);
+  currentQuestion = triviaQuestions[randomIndex];
+}
+
+function gameOver() {
+  // Determine the user with the highest score
+  let winner = null;
+  let highestScore = 0;
+  Object.entries(scores).forEach(([user, score]) => {
+    if (score > highestScore) {
+      highestScore = score;
+      winner = user;
+    }
+  });
+
+  // Remove all questions from the triviaQuestions array
+  triviaQuestions = [];
+
+  // Display a message to all users announcing the winner
+  io.emit("gameOver", {
+    winner: winner,
+    scores: scores,
+    restart: true,
+  });
+}
 
 http.listen(port, () => {
-  console.log('listening on port ', port)
-})
+  console.log("listening on http://localhost:" + port);
+});
--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,102 @@
 let socket = io();
 let messages = document.querySelector("section ul");
 let input = document.querySelector("input");
+let currentQuestion = null;
 
-const usernameSection = document.querySelector(".index");
-const formName = document.querySelector("section > form");
-const username = document.querySelector("#name");
+const username = window.prompt("Enter username");
+socket.emit("newUser", username);
 
-const usernames = document.querySelector("header ul");
-
-const users = (user) => {
-  const li = document.createElement("li");
-  li.setAttribute("id", `text${user.id}`);
-  li.innerHTML = `<p>${user.username}</p>`;
-  usernames.appendChild(li);
-};
-
-const submitUser = (user) => {
+document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
-  const value = input.value;
-  
-    socket.emit("name", value);
+  if (input.value) {
+    socket.emit("newMsg", input.value);
     input.value = "";
-    // usernameSection
-  };
+  }
+});
 
-const handleName = (user) => {
-  users(user);
-};
+socket.on("sendMessage", (message) => {
+  messages.appendChild(
+    Object.assign(document.createElement("li"), {
+      textContent: message.user + " : " + message.message,
+    })
+  );
+  messages.scrollTop = messages.scrollHeight;
+});
 
-formName.addEventListener("submit", submitUser);
+// handle the "updateUserList" event emitted by the server
+socket.on("updateUserList", (userList) => {
+  const userListElement = document.getElementById("user-list");
+  userListElement.innerHTML = ""; // clear the previous user list
 
+  for (let i = 0; i < userList.length; i++) {
+    const userElement = document.createElement("li");
+    const username = document.createTextNode(userList[i]);
+    userElement.appendChild(username);
+    userListElement.appendChild(userElement);
+  }
+});
+
+socket.on("newQuestion", (question) => {
+  currentQuestion = question;
+  const questionElement = document.getElementById("question");
+  questionElement.textContent = currentQuestion.question;
+
+  const answerElement = document.getElementById("answers");
+  answerElement.innerHTML = "";
+
+  //Create a button for each answer option
+  currentQuestion.allAnswers.forEach((answer) => {
+    const buttonElement = document.createElement("button");
+    buttonElement.textContent = answer;
+    answerElement.appendChild(buttonElement);
+
+    // Add click event listener to each button
+    buttonElement.addEventListener("click", () => {
+      socket.emit("answerSelected", answer);
+    });
+  });
+});
+
+socket.on("correctAnswer", (data) => {
+  messages.appendChild(
+    Object.assign(document.createElement("li"), {
+      textContent: `${data.user} guessed the correct answer (${
+        data.answer
+      })! Scores: ${JSON.stringify(data.scores)}`,
+      className: "system-message",
+    })
+  );
+  messages.scrollTop = messages.scrollHeight;
+});
+
+socket.on("gameOver", ({ winner, scores, restart }) => {
+  const message = winner
+    ? `${winner} wins with ${scores[winner]} points!`
+    : "Game over!";
+
+  // Remove the question and answers elements from the DOM
+  const questionElement = document.getElementById("question");
+  questionElement.remove();
+  const answerElement = document.getElementById("answers");
+  answerElement.remove();
+
+  const gameOverElement = Object.assign(document.createElement("div"), {
+    id: "game-over",
+    innerHTML: `<p>${message}</p>`,
+  });
+
+  if (restart) {
+    const restartButton = Object.assign(document.createElement("button"), {
+      textContent: "Restart",
+      onclick: () => {
+        socket.emit("restartGame");
+        gameOverElement.remove();
+        
+      },
+    });
+    gameOverElement.appendChild(restartButton);
+  }
+  // Add the game over element to the trivia
+  const triviaSection = document.querySelector(".trivia");
+  triviaSection.appendChild(gameOverElement);
+});
